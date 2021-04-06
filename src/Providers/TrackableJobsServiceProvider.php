3f8751e37945ee6c7b24ca97b96e523e594a1e02
Mateus Junges <mateus@junges.dev>
2021-04-06 14:42:08 -0300

    wip

--- src/Providers/TrackableJobsServiceProvider.php
+++ src/Providers/TrackableJobsServiceProvider.php
@@ -8,7 +8,11 @@ class TrackableJobsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/../config/trackable-jobs.php' => config_path('trackable-jobs.php'),
+        ], 'trackable-jobs-config');
+
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 
     public function register()
