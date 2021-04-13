2f8bb8d67bd1b3518bec7ef85dd2458ecf63e61c
Mateus Junges <mateus@junges.dev>
2021-04-13 11:58:04 -0300

    fixes from styleci

--- src/Contracts/TrackableJobContract.php
+++ src/Contracts/TrackableJobContract.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 interface TrackableJobContract
 {
-    public function trackable() : MorphTo;
+    public function trackable(): MorphTo;
 
-    public function markAsStarted() : bool;
+    public function markAsStarted(): bool;
 
-    public function markAsFinished(string $message = null) : bool;
+    public function markAsFinished(string $message = null): bool;
 
-    public function markAsFailed(string $exception = null) : bool;
+    public function markAsFailed(string $exception = null): bool;
 
     public function setOutput(string $output): bool;
 }
