4b984100dd85861de34d47b928d85cbc0e8c7d70
Mateus Junges <mateus@junges.dev>
2021-04-13 11:57:29 -0300

    fixes from styleci

--- config/trackable-jobs.php
+++ config/trackable-jobs.php
@@ -5,7 +5,7 @@ return [
      | The table where the tracked jobs will be stored.
      | By default, it's called 'tracked_jobs'.
      */
-    'tables' => array(
+    'tables' => [
         'tracked_jobs' => 'tracked_jobs',
-    ),
+    ],
 ];
