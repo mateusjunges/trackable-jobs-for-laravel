b86096ea61bec425818ea77595c4f06272b0ba64
AkioSarkiz <akiosarkiz@gmail.com>
2021-07-11 15:29:36 +0300

    reformat

--- tests/Jobs/Trackable/TestJob.php
+++ tests/Jobs/Trackable/TestJob.php
@@ -9,7 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Junges\TrackableJobs\Traits\Trackable;
 use Spatie\TestTime\TestTime;
 
 /**
