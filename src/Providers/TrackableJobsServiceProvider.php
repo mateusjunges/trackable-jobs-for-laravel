00e5f2000aa4447f495a2418126cc56681d9862b
Bram van Rijswijk <b.vanrijswijk@outlook.com>
2023-02-08 12:24:15 +0100

    feat: publish migration file

--- src/Providers/TrackableJobsServiceProvider.php
+++ src/Providers/TrackableJobsServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Junges\TrackableJobs\Providers;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Junges\TrackableJobs\Contracts\TrackableJobContract;
 use Junges\TrackableJobs\Models\TrackedJob;
@@ -12,13 +14,34 @@ class TrackableJobsServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/../../config/trackable-jobs.php' => config_path('trackable-jobs.php'),
-        ], 'trackable-jobs-config');
+        ], 'trackable-jobs-assets');
 
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->publishes([
+            __DIR__.'/../../database/migrations/2021_04_16_005274_laravel_trackable_create_tracked_jobs_table.php' => $this->getMigrationFileName('create_tracked_jobs_table.php'),
+        ], 'trackable-jobs-assets');
     }
 
     public function register()
     {
         $this->app->bind(TrackableJobContract::class, TrackedJob::class);
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @return string
+     */
+    protected function getMigrationFileName($migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem, $migrationFileName) {
+                return $filesystem->glob($path.'*_'.$migrationFileName);
+            })
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
