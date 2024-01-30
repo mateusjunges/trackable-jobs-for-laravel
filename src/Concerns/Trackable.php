eec5eae46ee43cf8bc34664192456a0dc5492b48
Mateus Junges <mateus@junges.dev>
2024-01-30 14:08:28 -0300

    Attempt to fix should be unique bug

--- src/Concerns/Trackable.php
+++ src/Concerns/Trackable.php
@@ -2,6 +2,10 @@
 
 namespace Junges\TrackableJobs\Concerns;
 
+use Illuminate\Bus\UniqueLock;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Bus\PendingDispatch;
 use Junges\TrackableJobs\Jobs\Middleware\TrackedJobMiddleware;
 use Junges\TrackableJobs\Models\TrackedJob;
@@ -19,12 +23,24 @@ trait Trackable
             return;
         }
 
+        // If this job implements the ShouldBeUnique, and it is a duplicate, laravel
+        // will discard the job from the queue. Therefore, there is no need to
+        // continue tracking this job, and we can safely return early.
+        if ($this instanceof ShouldBeUnique && ! $this->isUniqueJobAndCanAcquireLock()) {
+            return;
+        }
+
         $this->trackedJob = TrackedJob::create([
             'trackable_id' => $this->trackableKey(),
             'status' => null,
             'trackable_type' => $this->trackableType(),
             'name' => static::class,
         ]);
+    }
+
+    private function isUniqueJobAndCanAcquireLock(): bool
+    {
+        return (new UniqueLock(Container::getInstance()->make(Cache::class)))->acquire($this->job);
     }
 
     protected function trackableKey(): ?string
