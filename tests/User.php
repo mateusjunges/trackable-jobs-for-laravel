2c967624db19c087a53964c028a27195d61087f8
Mateus Junges <mateus@junges.dev>
2024-01-30 13:43:41 -0300

    Simplify test classes

--- tests/User.php
+++ tests/User.php
@@ -3,31 +3,13 @@
 namespace Junges\TrackableJobs\Tests;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-/**
- * Class User.
- *
- * @mixin Builder
- */
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = ['name', 'email'];
-
-    /**
-     * @var bool
-     */
     public $timestamps = false;
-
-    /**
-     * @var string
-     */
     protected $table = 'test_users';
 }
