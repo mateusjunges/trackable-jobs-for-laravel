d0a2ca239429139b30ed6c9e4b60684b20f7dfb2
Mateus Junges <mateus@junges.dev>
2024-01-30 13:42:58 -0300

    Add listener to update tracked job status

---
+++ src/Listeners/UpdateTrackedJobStatus.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Junges\TrackableJobs\Listeners;
+
+use Illuminate\Queue\Events\JobQueued;
+use Junges\TrackableJobs\Contracts\TrackableJobContract;
+
+final class UpdateTrackedJobStatus
+{
+    public function handle(JobQueued $event): void
+    {
+        if (isset($event->job->trackedJob)) {
+            $trackedJob = $event->job->trackedJob;
+            assert($trackedJob instanceof TrackableJobContract);
+
+            $trackedJob->markAsQueued($event->id);
+        }
+    }
+}
