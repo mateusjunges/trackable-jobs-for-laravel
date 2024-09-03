6a0374f37db22f7d87015c9b91283ade45616482
DimaVIII <DimaVIII@users.noreply.github.com>
2024-09-03 19:09:52 +0700

    Play with Queue Payload

--- src/Providers/TrackableJobsServiceProvider.php
+++ src/Providers/TrackableJobsServiceProvider.php
@@ -13,15 +13,35 @@ class TrackableJobsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../../config/trackable-jobs.php' => config_path('trackable-jobs.php'),
+            __DIR__ . '/../../config/trackable-jobs.php' => config_path('trackable-jobs.php'),
         ], 'trackable-jobs-config');
 
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         Event::listen(
             \Illuminate\Queue\Events\JobQueued::class,
             \Junges\TrackableJobs\Listeners\QueueTrackedJob::class
         );
+
+        // Injection of trackedJob into the payload
+//        \Illuminate\Queue\Queue::createPayloadUsing(function ($connection, $queue, $payload) {
+//
+//            $jobDataCommand = $payload['data']['command'];
+//            if (isset($jobDataCommand->trackable))
+//            {
+//                $trackedJob = TrackedJob::create([
+//                    'trackable_id' => $jobDataCommand->trackable ? $jobDataCommand->trackable->id ?? $jobDataCommand->trackable->uuid : null,
+//                    'trackable_type' => $jobDataCommand->trackable?->getMorphClass(),
+//                    'job_id' => $payload['id'],
+//                    'name' => get_class($jobDataCommand),
+//                    'name' => $payload['displayName'],
+//                ]);
+//
+//                return ['trackedJobId' => $trackedJob->id];
+//            }
+//
+//            return [];
+//        });
     }
 
     public function register(): void
